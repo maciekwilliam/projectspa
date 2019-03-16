8897c6bfaa22e7aafc9bc7c63dca4e0fc80678ba
maciekwilliam <maciejwillem@gmail.com>
2019-03-16 12:34:42 +0100

    Updates

---
+++ precache-manifest.e33704ba84866eef1596f9bb019a0fed.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "d9d36b65a132d26aed884ad58e4719eb",
+    "url": "/projectspa/static/media/header3.d9d36b65.jpg"
+  },
+  {
+    "revision": "33eca35fceea8311d67358a6629fa9d9",
+    "url": "/projectspa/static/media/header2.33eca35f.jpg"
+  },
+  {
+    "revision": "70e6670ee746fc59f414fde4fcce93af",
+    "url": "/projectspa/static/media/header1.70e6670e.jpg"
+  },
+  {
+    "revision": "f1d139d4dcff06686fd0",
+    "url": "/projectspa/static/js/runtime~main.a579ecde.js"
+  },
+  {
+    "revision": "7a1d7107d3e02a3e3ac4",
+    "url": "/projectspa/static/js/main.2da51b31.chunk.js"
+  },
+  {
+    "revision": "547d4af2c91db877e596",
+    "url": "/projectspa/static/js/2.402d1047.chunk.js"
+  },
+  {
+    "revision": "7a1d7107d3e02a3e3ac4",
+    "url": "/projectspa/static/css/main.c2918643.chunk.css"
+  },
+  {
+    "revision": "9228db23e891471e58c2c07a2a9df79a",
+    "url": "/projectspa/index.html"
+  }
+];
